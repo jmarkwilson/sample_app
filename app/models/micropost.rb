607ff349089c4a690158030afcd2213186e52e76
jmarkwilson <j_mark_wilson@yahoo.com>
2013-05-24 07:22:50 -0700

    complete 10.1.4 Micropost refinements

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,9 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content
-#  attr_protected :user_id
   belongs_to :user
 
   validates :user_id, presence: true
+
+  default_scope order: 'microposts.created_at DESC'
+  
 end
