43a589327968e98977effad752874452105545db
jmarkwilson <j_mark_wilson@yahoo.com>
2013-05-28 07:45:17 -0700

    complete 10.3.1 Access control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,14 @@ class UsersController < ApplicationController
   before_filter :admin_user,      only: :destroy
   before_filter :logged_in,       only: [:new, :create]
 
-  	def new
-        @user = User.new
-  	end
+	def new
+      @user = User.new
+	end
 
-  	def show
-  		@user = User.find(params[:id])
-  	end
+	def show
+		@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+	end
 
 	def create
   		@user = User.new(params[:user])
@@ -52,13 +53,8 @@ class UsersController < ApplicationController
 
   private
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
+    #moved signed_in_user to session_helper
+    
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -102,6 +102,20 @@ describe "Authentication" do
 					it { should have_selector('title', test: 'Sign In') }
 				end
 			end
+
+			describe "in the Microposts controller" do
+
+				describe "submitting to the create action form" do
+					before { post microposts_path }
+					specify { response.should redirect_to(signin_path) }
+				end
+
+				describe "submitting to the destroy action" do 
+					before { delete micropost_path(FactoryGirl.create(:micropost))}
+					specify { response.should redirect_to(signin_path) }
+				end
+			end
+
 		end
 
 		describe "as wrong user" do
